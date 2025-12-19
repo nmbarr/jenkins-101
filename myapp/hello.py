38a7ae90ad766394fa24ecc97b4d7869fbff36bf
Nicholas Barr <nickbarr10@gmail.com>
2025-12-18 22:41:45 -0800

    Updated function call in Jenkinsfile and changed hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,12 @@
-import fire
+import argparse
 
 def hello(name="World"):
-  return "Hello %s!" % name
+    return "Hello %s!" % name
 
 if __name__ == '__main__':
-  fire.Fire(hello)
+    parser = argparse.ArgumentParser(description='Hello greeting script')
+    parser.add_argument('name', nargs='?', default='World', help='Name to greet (default: World)')
+    args = parser.parse_args()
+    
+    result = hello(args.name)
+    print(result)
